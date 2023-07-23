88a3efed9ff4037a7d97bb5ab33c0bc594593f32
Viktor Hajdu <vi.h.seo@gmail.com>
2023-07-23 02:03:15 +0200

    add App + navigation container

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const App: React.FC = () => {
+
+  return (
+    <NavigationContainer>
+        <View>ey </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
