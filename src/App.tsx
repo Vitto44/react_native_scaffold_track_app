4d002abd4f4b006fa51c2da67357ca9b06d9092f
Viktor Hajdu <vi.h.seo@gmail.com>
2023-08-04 03:25:32 +0200

    hook it to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-
+import ListScreen from './components/list/listTab';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import CalendarScreen from './components/calendar/calendarTab';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
 
   return (
     <NavigationContainer>
-        <View>ey </View>
+        <Tab.Navigator screenOptions={{headerShown: false, }}>
+          <Tab.Screen 
+            options={{
+              tabBarLabel: 'Calendar',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="calendar" color={color} size={size} />
+              ),
+            }}
+            name="Calendar"
+            component={CalendarScreen}
+          />
+          <Tab.Screen 
+            options={{
+              tabBarLabel: 'Materials',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="list" color={color} size={size} />
+              ),
+            }}
+            name="Materials"
+            component={ListScreen} />
+        </Tab.Navigator>
     </NavigationContainer>
   );
 };
